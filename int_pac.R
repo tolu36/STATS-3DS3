e749ff67ceeeeeeb8d68f835e5e4ee78b7fb7a40
tolu36 <t.olusooto@gmail.com>
2021-01-17 20:35:36 -0500

    First Commit

---
+++ int_pac.R
@@ -0,0 +1,8 @@
+install.packages("rmarkdown")
+install.packages("knitr")
+install.packages("tinytex")
+install.packages("here")
+install.packages("tidyverse")
+install.packages("broom")
+install.packages("fs")
+install.packages("usethis")
